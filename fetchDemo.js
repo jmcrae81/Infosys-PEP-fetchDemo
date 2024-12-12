83753926d50266df32611ebb5d6d23f675e0e48d
jmcrae81 <jimmy.a.mcrae@gmail.com>
2024-12-12 01:39:59 -0500

    v0.1

--- fetchDemo.js
+++ fetchDemo.js
@@ -1,12 +1,19 @@
 const getJSON = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/2");
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
     const data = response.json();
     return data;
 }
 getJSON().then( (data)=>{
-    
-    //var randomString = JSON.parse(data);
+    const listOfTodos = document.querySelector("ul");
+    data.forEach(todos =>{
+        var userId = todos.userId;
+        var id = todos.id;
+        var title = todos.title;
+        var completed = todos.completed;
 
-    console.log(data)
+        listOfTodos.insertAdjacentHTML('beforeend', `<li>${userId} ${id} ${title} ${completed} </li>`);
+        
+    })
+   
 })
 
